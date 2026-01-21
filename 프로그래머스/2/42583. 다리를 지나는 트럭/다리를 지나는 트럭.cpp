3c1c3d98bcde74f0d7d8dee07373a767aa1be687
ssh07037 <107997128+ssh07037@users.noreply.github.com>
2026-01-21 13:48:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.07 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,37 +1,48 @@
 #include <string>
-#include <vector>
 #include <queue>
+#include <iostream>
+#include <vector>
+
 using namespace std;
+void printQueue(queue<int> q) {
+    while (!q.empty()) {
+        cout << "(" << q.front() << ")" << endl;
+        q.pop();
+    }
+    cout << endl;
+}
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    queue<int> bridge;
-    int time = 0, sum = 0;
-    int index = 0;
-
-    // 초기 bridge에 0으로 채워 넣음 (길이만큼 자리 유지)
-    for (int i = 0; i < bridge_length; i++) {
-        bridge.push(0);
+    int answer = 0;
+    
+    queue<int>q;
+    for(int i=0; i<bridge_length; i++)
+    {
+        q.push(0);
     }
-
-    while (index < truck_weights.size()) {
-        // 다리 앞쪽 트럭이 나감
-        sum -= bridge.front();
-        bridge.pop();
-
-        // 다음 트럭을 올릴 수 있으면 올림
-        if (sum + truck_weights[index] <= weight) {
-            bridge.push(truck_weights[index]);
+    
+    int index = 0;
+    int sum = 0;
+    while(index < truck_weights.size())
+    {
+        answer++;
+        sum -= q.front(); //다리 위 트럭무게
+        q.pop();
+        
+        if(sum <= weight && sum + truck_weights[index] <= weight)
+        {
+            q.push(truck_weights[index]);
             sum += truck_weights[index];
             index++;
-        } else {
-            bridge.push(0); // 아무 트럭도 못 올라가면 빈 자리
         }
+        else
+        {
+            q.push(0);
+        }
+        
+        //printQueue(q);
 
-        time++;
     }
-
-    // 마지막 트럭이 올라간 후 다리 끝까지 가는 시간 추가
-    time += bridge_length;
-
-    return time;
+    answer += q.size();
+    return answer;
 }
