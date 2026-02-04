f0f26f1c7bc218256901dc1ac48e36ec604e7de3
ssh07037 <107997128+ssh07037@users.noreply.github.com>
2026-02-04 12:29:51 +0900

    [level 3] Title: 섬 연결하기, Time: 0.18 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,50 +1,53 @@
+#include <string>
 #include <vector>
 #include <climits>
+#include <iostream>
 using namespace std;
 
-bool visited[100];
 int min_cost[100];
-
+bool check[100];
 int solution(int n, vector<vector<int>> costs) {
-    // 그래프 구성
-    vector<vector<pair<int,int>>> graph(n);
-    for (auto& c : costs) {
-        graph[c[0]].push_back({c[1], c[2]}); //0 : 1,1  2,2 ...
-                                             //1 : 0,1 2,5 .... 각 지점에서 갈 수 있는 곳과 가격 알아냄
-        graph[c[1]].push_back({c[0], c[2]});
-    }
-
-    // 초기화
-    for (int i = 0; i < n; i++)
-        min_cost[i] = INT_MAX;
-
-    min_cost[0] = 0;  // 0번 섬에서 시작
     int answer = 0;
-
-    for (int i = 0; i < n; i++) {
-        int cur = -1;
-        int cost = INT_MAX;
-
-        // 아직 방문 안 한 섬 중 최소 비용 섬 선택
-        for (int j = 0; j < n; j++) {
-            if (!visited[j] && min_cost[j] < cost) { //방문하지않았고 가격이 싸다면
-                cost = min_cost[j];
-                cur = j;
-            } //0->1로 비용 1
-        }
-
-        visited[cur] = true;
-        answer += cost;
-
-        // 연결된 섬 비용 갱신
-        for (auto& next : graph[cur]) {
-            int nextNode = next.first;
-            int nextCost = next.second;
-            if (!visited[nextNode] && min_cost[nextNode] > nextCost) {
-                min_cost[nextNode] = nextCost;
+    
+    vector<vector<pair<int,int>>> graph(n);
+    for (auto& a : costs) 
+    {
+        graph[a[0]].push_back(make_pair(a[1],a[2]));
+        graph[a[1]].push_back(make_pair(a[0],a[2]));
+    }
+    for (int i = 0; i < n; i++)
+    {
+        min_cost[i] = INT_MAX;
+    }
+    min_cost[0] = 0;
+    
+    for(int i=0; i<n; i++)
+    {
+        int min = INT_MAX;
+        int index = 0;
+        for(int j=0; j<n; j++) //최소값 찾기
+        {
+            if(min > min_cost[j] && check[j] == false)
+            {
+                min = min_cost[j];
+                //cout << min << endl;
+                index = j;
             }
         }
+        check[index] = true;
+        
+        for(auto a : graph[index]) //갱신
+        {
+            int next = a.first;
+            cout << next << endl;
+            int next_cost = a.second;
+            if(min_cost[next] > next_cost)
+            {
+                min_cost[next] = next_cost;
+            }
+        }
+        answer += min;
+        
     }
-
     return answer;
 }
