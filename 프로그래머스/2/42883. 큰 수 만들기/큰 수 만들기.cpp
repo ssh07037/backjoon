70cba6bf14b734e79274c8d2b3df0077c91b8845
ssh07037 <107997128+ssh07037@users.noreply.github.com>
2026-02-04 10:29:17 +0900

    [level 2] Title: 큰 수 만들기, Time: 4.57 ms, Memory: 5.98 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,71 +1,47 @@
 #include <string>
 #include <vector>
-#include<deque>
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    deque<char>a;
-    a.push_back(number[0]);
-    int count = 0;
-    int index = 1;
+    //인간 관점에서 k수 + 1만큼 범위에서 가장 큰 값이 있는 위치까지 제거
+    //k=3 4185 -> 4,1제거
     
-    //k와 number수가 같다면
-    //2 2
-    while(count != k) //a.size()가 2가 아니면
+    //다음값이 더 크면 제거?
+    
+    vector<int>stack;
+    int index =0;
+    stack.push_back(number[index] - '0');
+    
+    while(index < number.size()-1 && k > 0)
     {
-        if(!a.empty() && a.back() < number[index])  //41 , 7                                             
-        {
-            while (!a.empty() && a.back() < number[index]) 
-            {
-                    a.pop_back(); //4 , 7
-                    count++;
-                    if(count == k)
-                    {
-                        break;
-                    }
-            }
-        }
-         a.push_back(number[index]); //98754 3214
-         index++;
-    }
-    //8개 값이 들어감
-    cout << a.size() << endl;
-    while(index < number.size())
-    {
-        a.push_back(number[index]);
         index++;
+        while( !stack.empty() && k>0 && stack.back() < number[index]- '0')
+        {
+            stack.pop_back();
+            k--;
+        }
+        stack.push_back(number[index]- '0');
     }
-    count = 0;
-    while(count != number.size()-k)
+    
+    //for(auto a : stack)
+    //    cout << a << endl;
+    
+    //cout << "남은횟수 " << k << endl;
+    while(index < number.size()-1)
     {
-        
-        answer += a.front();
-        a.pop_front();
-        count++;
+        index++;
+        stack.push_back(number[index]- '0');
     }
+    while(k > 0)
+    {
+        stack.pop_back();
+        k--;
+    }
+    for(auto a : stack)
+        answer += a + '0';
+    // k남으면 처리
     return answer;
-    //98759812 k=1 이면 5를 지우고
-    //k=2면 1,2를 지워야한다
-    
-    //작아지면 상관없고 뒷값이 커지면 상관있다 앞값을 지운다
-    
-    //4177252841
-    //417 문제발생 1을 지움
-    //47 문제발생 4를 지움
-    //7725 문제발생 2를 지움
-    //77528 문제발생 2를 지움
-    //7758
-    
-    //1924
-    //19 문제발생 1을 지움
-    //924 문제발생 2를지움
-    //94
-    
-    //1231234
-    //12 문제발생 1을 지움
-    //23 문제발생 2을 지움
-    //312 문제발생 1을 지움
-   
 }
