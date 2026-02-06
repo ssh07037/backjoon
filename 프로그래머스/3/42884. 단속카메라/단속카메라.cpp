91b7af4ff4b4dd9e0815fe74a0c590803c851b9c
ssh07037 <107997128+ssh07037@users.noreply.github.com>
2026-02-06 12:14:54 +0900

    [level 3] Title: 단속카메라, Time: 0.88 ms, Memory: 4.28 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,25 +1,26 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
+
 int solution(vector<vector<int>> routes) {
-    sort(routes.begin(), routes.end(), [](auto& a, auto& b) {
-        return a[1] < b[1]; // 나가는 지점 기준 정렬
+    int answer = 0;
+    
+    sort(routes.begin(),routes.end(),[](auto& a , auto& b){
+        return a[1] < b[1];
     });
+    
+   
 
     
-    int answer = 0;
-    int camera = -30001;
-    
-    
-    //끝점과 시작점 비교 시작점이 끝점보다 크면 새로운 카메라 설치
-    for (auto& r : routes) 
+    int min = -30001;
+    for(auto a : routes)
     {
-        if (camera < r[0]) 
-        {  
-            camera = r[1];    
+        if(min < a[0])
+        {
+            cout << min << " " << a[0] << endl;
+            min = a[1];
             answer++;
         }
     }
