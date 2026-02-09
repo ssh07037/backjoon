c36788496e1bbb39b920c9d15cf51b42e867208a
ssh07037 <107997128+ssh07037@users.noreply.github.com>
2026-02-09 13:15:06 +0900

    [level 3] Title: 등굣길, Time: 0.06 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    
+    int dp[101][101] = {0};
+    for(auto a : puddles)
+    {
+        dp[a[1]][a[0]] = -1;
+    }
+    dp[1][1] = 1;
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=m; j++)
+        {
+            if(i==1 && j==1)
+            {
+                continue;
+            }
+            if(dp[i][j] == -1)
+            {
+                dp[i][j] = 0;
+            }
+            else
+            {
+                dp[i][j] =(dp[i-1][j] + dp[i][j-1]) % 1000000007;
+            }
+        }
+    }
+    
+ 
+    answer = dp[n][m] % 1000000007;
+    return answer;
+}
