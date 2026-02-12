9547ab03e41f5f47b0f4860edbc77c20e1e4915b
ssh07037 <107997128+ssh07037@users.noreply.github.com>
2026-02-12 13:24:53 +0900

    [level 2] Title: 타겟 넘버, Time: 214.74 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,22 +1,45 @@
+#include <string>
 #include <vector>
+#include <iostream>
+
 using namespace std;
-
-int answer = 0;
-
-void dfs(const vector<int>& numbers, int index, int current, int target) {
-    if(index == numbers.size()) 
+int counts;
+void dfs(const vector<int>& numbers, int target,int current_number , int count,string operate,vector<int>temp)
+{
+    if(operate == "plus")
     {
-        if(current == target) 
-            answer++;
+        current_number += numbers[count];
+        temp.push_back(numbers[count]);
+    }
+    else if(operate == "minus")
+    {
+        current_number -= numbers[count];
+        temp.push_back(numbers[count] * -1);
+    }
+    count++;
+    if(count == numbers.size() && current_number == target)
+    {
+      //  for(auto a : temp)
+      //      cout << a << endl;
+        //cout << endl;
+        counts++;
         return;
     }
-    dfs(numbers, index + 1, current + numbers[index], target);
-    dfs(numbers, index + 1, current - numbers[index], target);
+    else if(count == numbers.size())
+    {
+        return;
+    }
+    
+     dfs(numbers,target,current_number,count,"plus",temp);
+    dfs(numbers,target,current_number,count,"minus",temp);
+    
+    return;
 }
-
-int solution(vector<int> numbers, int target) 
-{
-    answer = 0;
-    dfs(numbers, 0, 0, target);
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    vector<int>temp;
+    dfs(numbers,target,0,0,"plus",temp);
+    dfs(numbers,target,0,0,"minus",temp);
+    answer = counts;
     return answer;
 }
